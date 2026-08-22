66b1a627e7ee8d6d9161e65b8a46a7abc6eaa20c
mihirmendake-crypto <mihirmendake@gmail.com>
2026-08-23 03:13:25 +0530

    Use of include tags

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ from . import views
 #     path('allposts/python_oops',views.python_oops),
 
 urlpatterns = [
-    path('',views.home_page),
+    path('',views.home_page, name="home"),
     path('allposts', views.blogposts),
     #path("allposts/<int:blog>", views.blog_post_by_number),
     path("allposts/<slug:blog>",views.blog_post, name="blog-post"),
